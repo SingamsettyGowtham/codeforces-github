9133df5a5a5d960585cd2290e56f0207b1e0be69
Gowtham <157223165+SingamsettyGowtham@users.noreply.github.com>
2026-08-01 13:11:22 +0530

    Add Creating Words [A] from Codeforces

---
+++ 1985/A - Creating Words/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string a, b, temp;
+        cin >> a >> b;
+        temp[0] = a[0];
+        a[0] = b[0];
+        b[0] = temp[0];
+        cout << a << " " << b << endl;
+    }
+}
