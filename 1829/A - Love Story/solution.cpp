31fa608048cc78068b47619aa1faf866f1a110c1
Gowtham <157223165+SingamsettyGowtham@users.noreply.github.com>
2026-08-01 13:12:23 +0530

    Add Love Story [A] from Codeforces

---
+++ 1829/A - Love Story/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t; cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        
+        string s1 = "codeforces";
+        int c =0;
+        
+        for(int i=0; i<10; i++){
+            if(s[i] != s1[i]) c++;
+        }
+        cout << c <<endl;
+    }
+}
