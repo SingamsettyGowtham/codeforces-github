1b2fe51713dd53588d912b30e7e93c138b6f9d7c
Gowtham <157223165+SingamsettyGowtham@users.noreply.github.com>
2026-08-01 13:10:22 +0530

    Add Amusing Joke [A] from Codeforces

---
+++ 141/A - Amusing Joke/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    string a, b , c;
+    cin >> a >> b >> c;
+    
+    sort(c.begin(), c.end());
+    
+    string d = a + b;
+    sort(d.begin(), d.end());
+    
+    
+    if(d == c) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
