1a55fa79ff7928b87e3b74e4d83b1661f850a196
Gowtham <157223165+SingamsettyGowtham@users.noreply.github.com>
2026-08-01 13:08:22 +0530

    Add Restoring Three Numbers [A] from Codeforces

---
+++ 1154/A - Restoring Three Numbers/solution.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    vector<int> v(4);
+    for(int i=0; i<4; i++) cin >> v[i];
+    sort(v.begin(), v.end());
+    for(int i=0; i<v.size()-1; i++) cout << v[3] - v[i] << " ";
+}
