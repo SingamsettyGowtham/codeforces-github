6a224099e495aecde8b8cb2bf7a3212d246dd9b2
Gowtham <157223165+SingamsettyGowtham@users.noreply.github.com>
2026-08-06 11:18:21 +0530

    Add Primary Task [A] from Codeforces

---
+++ 2000/A - Primary Task/solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        if ((n >= 102 && n <= 109) || (n >= 1010 && n <= 1099)){
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
+}
